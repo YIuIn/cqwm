0dcde4e5294cf9092238cc35cb1b2fefa6a48e3c
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-08-06 09:04:50 +0800

    新增菜品业务功能开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,25 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
+    /**
+     * 根据分类查找菜品数量
+     * @param id
+     * @return
+     */
     @Select("select count(*) from dish where dish.category_id=#{id}")
     Integer countByCategoryId(Long id);
+
+    /**
+     * 插入菜品数据
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
